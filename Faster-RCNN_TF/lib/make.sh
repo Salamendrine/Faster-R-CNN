4c2818da366ca8bfaf960b283e3f65c691821f65
Haobo's Mac <gogoli@Haobos-MacBook.local>
2019-06-14 16:59:13 +0800

    Try

--- Faster-RCNN_TF/lib/make.sh
+++ Faster-RCNN_TF/lib/make.sh
@@ -14,9 +14,14 @@ if [ -d "$CUDA_PATH" ]; then
 		-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC $CXXFLAGS \
 		-arch=sm_37
 
-	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
-		roi_pooling_op.cu.o -I $TF_INC  -D GOOGLE_CUDA=1 -fPIC $CXXFLAGS \
-		-lcudart -L $CUDA_PATH/lib64
+   #  g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
+		# roi_pooling_op.cu.o -I $TF_INC  -D GOOGLE_CUDA=1 -fPIC $CXXFLAGS \
+		# -lcudart -L $CUDA_PATH/lib64
+
+    g++ -std=c++11 -shared -D_GLIBCXX_USE_CXX11_ABI=0 -o roi_pooling.so roi_pooling_op.cc\
+        roi_pooling_op.cu.o -I $TF_INC -D GOOGLE_CUDA=1 -fPIC $CXXFLAGS -D_GLIBCXX_USE_CXX11_ABI=0\
+        -lcudart -L $CUDA_PATH/lib64
+    
 else
 	g++ -std=c++11 -shared -o roi_pooling.so roi_pooling_op.cc \
 		-I $TF_INC -fPIC $CXXFLAGS
@@ -29,6 +34,6 @@ cd ..
 #nvcc -std=c++11 -c -o feature_extrapolating_op.cu.o feature_extrapolating_op_gpu.cu.cc \
 #	-I $TF_INC -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC -arch=sm_50
 
-#g++ -std=c++11 -shared -o feature_extrapolating.so feature_extrapolating_op.cc \
-#	feature_extrapolating_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
+# g++ -std=c++11 -shared -o feature_extrapolating.so feature_extrapolating# _op.cc \
+    # feature_extrapolating_op.cu.o -I $TF_INC -fPIC -lcudart -L $CUDA_PATH/lib64
 #cd ..
