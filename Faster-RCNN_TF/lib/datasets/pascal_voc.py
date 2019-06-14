53ea4c1c5bc514e96ca69b04deef8369f6b2d87f
Haobo's Mac <gogoli@Haobos-MacBook.local>
2019-06-14 13:35:23 +0800

    Try

--- Faster-RCNN_TF/lib/datasets/pascal_voc.py
+++ Faster-RCNN_TF/lib/datasets/pascal_voc.py
@@ -29,7 +29,7 @@ class pascal_voc(imdb):
         self._devkit_path = self._get_default_path() if devkit_path is None \
                             else devkit_path
         self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
-        self._classes = ('__background__', '铁壳打火机', '黑钉打火机','刀具', '电源和电池', '剪刀')
+        self._classes = ('__background__', '铁壳打火机'.encode(encoding='UTF-8',errors='strict'), '黑钉打火机'.encode(encoding='UTF-8',errors='strict'),'刀具'.encode(encoding='UTF-8',errors='strict'), '电源和电池'.encode(encoding='UTF-8',errors='strict'), '剪刀'.encode(encoding='UTF-8',errors='strict'))
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
